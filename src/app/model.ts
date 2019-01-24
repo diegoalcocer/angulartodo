3cbd5a723e8ace3d956a57f611322168c0c750f5
Alcocer Palacios, Diego <DIEGOAL@iadb.org>
2019-01-24 18:14:06 -0500

    DA: Use Angular to Populate the To Do list

---
+++ src/app/model.ts
@@ -0,0 +1,21 @@
+// var model = {
+//     user: "Adam",
+//     items: [{action: "Buy flowers", done:false},
+//     {action: "Get Shoes", done:false}]
+// };
+export class Model {
+    user: any;
+    items: any;
+    constructor(){
+        this.user = "Diego";
+        this.items = [new TodoItem("Buy Flowers", false), new TodoItem("Get Shoes", false)]
+    }
+}
+export class TodoItem{
+    action: any;
+    done: any;
+    constructor(action: string, done: boolean){
+        this.action = action;
+        this.done = done;
+    }
+}
